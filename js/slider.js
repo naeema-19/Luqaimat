604fd4d64ac398c9ba210480a4d5f31d9cfa8339
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-01-21 00:21:52 +0530

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+/**
+* Template Name: Laura - v4.10.0
+* Template URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+
+
+
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+ 
+  
+
+})()
